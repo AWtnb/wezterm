03be063a6cbd46a8fb18883e1c0fbc28db5d48d2
awtnb <arikaebanataw@gmail.com>
2023-09-13 12:05:53 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# README
+
+My [wezterm](https://wezfurlong.org/wezterm/) configuration.
+
+- `wezterm.lua` should be placed in `~\.config\wezterm` .
+- On Windows, the easist way is creating a junction towards the above folder.
+
+Thanks:
+- https://zenn.dev/yutakatay/articles/wezterm-intro
+- https://gist.github.com/sheepla/440dd9f82f08d31e6f109da3a2e65d4c
---
+++ wezterm.lua
@@ -0,0 +1,51 @@
+local wezterm = require 'wezterm';
+local config = {}
+
+config.font = wezterm.font_with_fallback({{
+    family = "HackGen",
+    weight = "Regular"
+}, {
+    family = "PlemolJP Console",
+    weight = "Medium"
+}, {
+    family = "MS Gothic"
+}})
+
+config.font_size = 14.0
+config.color_scheme = "Kanagawa (Gogh)"
+config.disable_default_key_bindings = true
+config.use_ime = true
+config.default_prog = {"pwsh.exe", "-WorkingDirectory", string.format("C:\\Users\\%s\\Desktop", os.getenv("USERNAME"))}
+
+config.default_cursor_style = "BlinkingUnderline"
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+
+-- https://wezfurlong.org/wezterm/config/default-keys.html
+config.keys = {{
+    key = "t",
+    mods = "CTRL",
+    action = wezterm.action {
+        SpawnTab = "CurrentPaneDomain"
+    }
+}, {
+    key = "Tab",
+    mods = "CTRL",
+    action = wezterm.action {
+        ActivateTabRelative = 1
+    }
+}, {
+    key = "Tab",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action {
+        ActivateTabRelative = -1
+    }
+}, {
+    key = "w",
+    mods = "CTRL",
+    action = wezterm.action.CloseCurrentTab {
+        confirm = true
+    }
+}}
+
+return config
