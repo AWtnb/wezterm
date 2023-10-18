8d8e3d536f80255d5aa37233c0b5a502d609277d
awtnb <arikaebanataw@gmail.com>
2023-10-18 15:02:51 +0900

    change theme

--- wezterm.lua
+++ wezterm.lua
@@ -22,7 +22,7 @@ config.front_end = "Software"
 config.hide_tab_bar_if_only_one_tab = true
 
 -- https://wezfurlong.org/wezterm/colorschemes/index.html
-config.color_scheme = "Bamboo"
+config.color_scheme = "Later This Evening (Gogh)"
 
 config.disable_default_key_bindings = true
 config.use_ime = true
