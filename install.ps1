f5fce321a1df38b55373b2bd807f09a4a15a4dd1
awtnb <46807238+AWtnb@users.noreply.github.com>
2026-08-16 13:22:36 +0900

    no confirm

--- install.ps1
+++ install.ps1
@@ -1,4 +1,5 @@
 $d = "wezterm"
 $dataPath = $env:USERPROFILE | Join-Path -ChildPath ".config" | Join-Path -ChildPath $d
+Remove-Item -Path $dataPath -Force -Recurse -ErrorAction SilentlyContinue > $null
 $srcPath = $PSScriptRoot | Join-Path -ChildPath $d
-New-Item -Path $dataPath -Value $srcPath -ItemType Junction -Confirm -Force
+New-Item -Path $dataPath -Value $srcPath -ItemType Junction -Force
